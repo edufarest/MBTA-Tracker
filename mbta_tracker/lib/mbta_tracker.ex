fa77e3785dc7fffa3a968e7ce1842b9161d019ad
Eduardo <eduardofares123@gmail.com>
2017-11-06 21:48:00 -0500

    API Experiment

--- mbta_tracker/lib/mbta_tracker.ex
+++ mbta_tracker/lib/mbta_tracker.ex
@@ -6,4 +6,24 @@ defmodule MbtaTracker do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  # Requests data from API
+  def getStop!(lat, lon) do
+
+    key = "v_ULBvHWlEK0J-_BMEPb_Q"
+    
+    resp = HTTPoison.get!("http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=#{key}&lat=#{lat}&lon=#{lon}&format=json")
+    data = Poison.decode!(resp.body)
+
+    # Clostest stop
+    stop = Enum.at(data["stop"], 0)
+    stop_id = stop["stop_id"]
+
+    # Get predictions
+
+    resp = HTTPoison.get!("http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=#{key}&stop=#{stop_id}&format=json")
+    data = Poison.decode!(resp.body)
+
+    route = Enum.at(data["mode"], 0)
+  end
 end
